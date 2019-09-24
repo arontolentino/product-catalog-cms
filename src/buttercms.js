455a8ad2cd3a1f87c4198f4fcfb1fac5bf7c20e3
Aron Tolentino <arontolentino@outlook.com>
2019-09-23 20:28:43 -0400

    Setup Vue.js project and ButterCMS

---
+++ src/buttercms.js
@@ -0,0 +1,2 @@
+import Butter from "buttercms";
+export const butter = Butter("bb8e6b3f415a02f3aa781af85d482ceee6a14719");
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div v-for="(liquor, index) in liquors" :key="index">
+      <h1>{{ liquor.product_name }}</h1>
+      <h2>{{ liquor.product_description }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import { butter } from "@/buttercms";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      liquors: []
+    };
+  },
+  methods: {
+    getProducts() {
+      butter.content.retrieve(["liquor"]).then(res => {
+        this.liquors = res.data.data.liquor;
+        console.log(this.liquors);
+      });
+    }
+  },
+  created() {
+    this.getProducts();
+  }
+};
+</script>
